8e7b584cf7468c76e87a9839a9a7ae0f74df8f59
Cynthia <cynthia-lam@outlook.com>
2024-06-25 00:15:11 -0400

    Move input to game

--- game.rb
+++ game.rb
@@ -1,10 +1,24 @@
+require './player'
+
 class Game
 
   def initialize
-    @current_player = Player1
+    puts "Player 1, what's your name?"
+    p1_name = $stdin.gets.chomp
+    @player1 = Player.new(p1_name)
+
+    puts "Player 2, what's your name?"
+    p2_name = $stdin.gets.chomp
+    @player2 = Player.new(p2_name)
+
+    @current_player = player1
 
   def change_turn
-    if
+    if @current_player = player1
+      @current_player = player2
+    end
+    if @current_player = player2
+      @current_player = player1
     end
   end
 end
--- main.rb
+++ main.rb
@@ -1,9 +1,4 @@
 require './player'
+require './game'
 
-puts "Player 1, what's your name?"
-p1_name = $stdin.gets.chomp
-player1 = Player.new(p1_name)
-
-puts "Player 2, what's your name?"
-p2_name = $stdin.gets.chomp
-player2 = Player.new(p2_name)
+start = Game.new
